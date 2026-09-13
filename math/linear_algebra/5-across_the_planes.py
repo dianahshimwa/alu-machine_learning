97b2b7b083627516ecea2f1831a1f2e797929fd4
dianahshimwa <d.shimwa@alustudent.com>
2026-09-13 10:57:46 -0700

    Add 5-across_the_planes.py 2D matrix element-wise addition

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module to add two 2D matrices element-wise."""
+
+
+def add_matrices2D(mat1, mat2):
+    """Add two 2D matrices element-wise, or return None if shapes differ."""
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+    return [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))]
+            for i in range(len(mat1))]
