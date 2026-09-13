f466324ad1dc04ab11efd3d3919dacb1a3639e45
dianahshimwa <d.shimwa@alustudent.com>
2026-09-13 11:52:51 -0700

    Add 14-saddle_up.py numpy matrix multiplication

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module to perform matrix multiplication with numpy."""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Return the matrix product of two numpy arrays."""
+    return np.matmul(mat1, mat2)
