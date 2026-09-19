defc25e0bbbe18ec3a59db56ef6eb9643632bdc4
dianahshimwa <d.shimwa@alustudent.com>
2026-09-19 07:20:45 -0700

    Add 5-definiteness.py matrix definiteness classification

---
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Module to calculate the definiteness of a matrix."""
+import numpy as np
+
+
+def definiteness(matrix):
+    """Calculate the definiteness of a matrix."""
+    if type(matrix) is not np.ndarray:
+        raise TypeError("matrix must be a numpy.ndarray")
+    if matrix.ndim != 2 or matrix.shape[0] != matrix.shape[1]:
+        return None
+    if matrix.size == 0:
+        return None
+    if not np.array_equal(matrix, matrix.T):
+        return None
+    eigenvalues =np.linalg.eigvals(matrix)
+    if np.all(eigenvalues > 0):
+        return "Positive definite"
+    if np.all(eigenvalues >= 0):
+        return "Positive semi-definite"
+    if np.all(eigenvalues < 0):
+        return "Negative definite"
+    if np.all(eigenvalues <= 0):
+        return "Negative semi-definite"
+    return "Indefinite"
